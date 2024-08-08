ee8cc64a361e01bde80dd80c2a4d8ec3f451fe12
Christopher Cortes <christopherctes@hotmail.com>
2024-08-08 19:13:35 -0400

    service page

--- app/nuestros-servicios/page.tsx
+++ app/nuestros-servicios/page.tsx
@@ -7,8 +7,18 @@ export default function Services() {
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-8">Nuestros Servicios</h1>
       </section>
 
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-16">
-        <div>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-10 xl:px-16">
+        <div className="text-center md:text-left">
+          <Image
+            src="/servicios/grua.jpeg"
+            width={700}
+            height={400}
+            alt="Grúa"
+            className="w-full h-auto object-cover rounded-lg mx-auto md:w-[500px] md:h-[300px] lg:w-[600px] lg:h-[350px] xl:w-[700px] xl:h-[400px]"
+            loading="lazy"
+          />
+        </div>
+        <div className="lg:max-w-[600px] lg:mx-auto xl:max-w-[700px] flex flex-col justify-center">
           <h2 className="text-2xl md:text-3xl font-semibold mb-4">Servicios de Grúa</h2>
           <h3 className="text-xl font-bold mb-4 text-gray-600">Asistencia en Ruta</h3>
           <p className="text-lg leading-relaxed">
@@ -17,20 +27,9 @@ export default function Services() {
             Solo debe llamarnos y nosotros coordinamos el servicio de grúa.
           </p>
         </div>
-        <div>
-          <Image
-            src="/servicios/grua.jpeg"
-            width={700}
-            height={400}
-            alt="Grúa"
-            className="w-full h-auto object-cover rounded-lg"
-            loading="lazy"
-          />
-        </div>
       </section>
-
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-16 mt-16">
-        <div>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-10 xl:px-16 mt-16">
+        <div className="order-last md:order-first flex flex-col justify-center">
           <h2 className="text-2xl md:text-3xl font-semibold mb-4">Servicio Scanner</h2>
           <h3 className="text-xl font-bold mb-4 text-gray-600">Chequeo OBD1 y OBD2</h3>
           <p className="text-lg leading-relaxed">
@@ -38,20 +37,29 @@ export default function Services() {
             se realiza en nuestro taller y también a domicilio dependiendo de las necesidades de nuestros clientes.
           </p>
         </div>
-        <div>
+        <div className="text-center md:text-left">
           <Image
             src="/servicios/cardiagnostic.jpg"
             width={700}
             height={400}
             alt="Servicio Scanner"
-            className="w-full h-auto object-cover rounded-lg"
+            className="w-full h-auto object-cover rounded-lg mx-auto md:w-[500px] md:h-[300px] lg:w-[600px] lg:h-[350px] xl:w-[700px] xl:h-[400px]"
             loading="lazy"
           />
         </div>
       </section>
-
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-16 mt-16">
-        <div>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-10 xl:px-16">
+        <div className="text-center md:text-left">
+          <Image
+            src="/servicios/pintura.jpeg"
+            width={700}
+            height={400}
+            alt="Servicio de Pintura"
+            className="w-full h-auto object-cover rounded-lg mx-auto md:w-[500px] md:h-[300px] lg:w-[600px] lg:h-[350px] xl:w-[700px] xl:h-[400px]"
+            loading="lazy"
+          />
+        </div>
+        <div className="lg:max-w-[600px] lg:mx-auto xl:max-w-[700px] flex flex-col justify-center">
           <h2 className="text-2xl md:text-3xl font-semibold mb-4">Servicio de Pintura</h2>
           <h3 className="text-xl font-bold mb-4 text-gray-600">Desabolladura y Pintura</h3>
           <p className="text-lg leading-relaxed">
@@ -60,20 +68,9 @@ export default function Services() {
             tanto en Desabolladura, pintura, parabrisas y vidrios.
           </p>
         </div>
-        <div>
-          <Image
-            src="/servicios/pintura.jpeg"
-            width={700}
-            height={400}
-            alt="Servicio de Pintura"
-            className="w-full h-auto object-cover rounded-lg"
-            loading="lazy"
-          />
-        </div>
       </section>
-
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-16 mt-16">
-        <div>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-10 xl:px-16 mt-16">
+        <div className="order-last md:order-first flex flex-col justify-center">
           <h2 className="text-2xl md:text-3xl font-semibold mb-4">Revisión Técnica</h2>
           <h3 className="text-xl font-bold mb-4 text-gray-600">Chequeo y trámite</h3>
           <p className="text-lg leading-relaxed">
@@ -83,42 +80,39 @@ export default function Services() {
             y lo entregamos aprobado.
           </p>
         </div>
-        <div>
+        <div className="text-center md:text-left">
           <Image
             src="/servicios/trabajador.jpg"
             width={700}
             height={400}
             alt="Revisión Técnica"
-            className="w-full h-auto object-cover rounded-lg"
+            className="w-full h-auto object-cover rounded-lg mx-auto md:w-[500px] md:h-[300px] lg:w-[600px] lg:h-[350px] xl:w-[700px] xl:h-[400px]"
             loading="lazy"
           />
         </div>
       </section>
-
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-16 mt-16">
-        <div>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-10 xl:px-16">
+        <div className="text-center md:text-left">
+          <Image
+            src="/servicios/verificado.jpeg"
+            width={700}
+            height={400}
+            alt="Permiso de Circulación"
+            className="w-full h-auto object-cover rounded-lg mx-auto md:w-[500px] md:h-[300px] lg:w-[600px] lg:h-[350px] xl:w-[700px] xl:h-[400px]"
+            loading="lazy"
+          />
+        </div>
+        <div className="lg:max-w-[600px] lg:mx-auto xl:max-w-[700px] flex flex-col justify-center">
           <h2 className="text-2xl md:text-3xl font-semibold mb-4">Permiso de Circulación</h2>
           <h3 className="text-xl font-bold mb-4 text-gray-600">Trámite Municipal</h3>
           <p className="text-lg leading-relaxed">
             En Rubinstein nos preocupamos y encargamos de que tu vehículo tenga los permisos municipales al día, para
             que puedas circular sin problemas, nos hacemos cargo del trámite municipal y así ahorres tiempo.
           </p>
-          <p className="text-medium font-bold mb-4">*Trámite no incluye costos de SOAP y Permiso de circulación.</p>
-        </div>
-        <div>
-          <Image
-            src="/servicios/verificado.jpeg"
-            width={700}
-            height={400}
-            alt="Permiso de Circulación"
-            className="w-full h-auto object-cover rounded-lg"
-            loading="lazy"
-          />
         </div>
       </section>
-
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-16 mt-16">
-        <div>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-10 xl:px-16 mt-16">
+        <div className="order-last md:order-first flex flex-col justify-center">
           <h2 className="text-2xl md:text-3xl font-semibold mb-4">Repuestos</h2>
           <h3 className="text-xl font-bold mb-4 text-gray-600">Stock de Repuestos</h3>
           <p className="text-lg leading-relaxed">
@@ -127,20 +121,29 @@ export default function Services() {
             para liberarte de estos trámites.
           </p>
         </div>
-        <div>
+        <div className="text-center md:text-left">
           <Image
             src="/servicios/engine.jpg"
             width={700}
             height={400}
             alt="Repuestos"
-            className="w-full h-auto object-cover rounded-lg"
+            className="w-full h-auto object-cover rounded-lg mx-auto md:w-[500px] md:h-[300px] lg:w-[600px] lg:h-[350px] xl:w-[700px] xl:h-[400px]"
             loading="lazy"
           />
         </div>
       </section>
-
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-16 mt-16">
-        <div>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-10 xl:px-16">
+        <div className="text-center md:text-left">
+          <Image
+            src="/servicios/oil.jpg"
+            width={700}
+            height={400}
+            alt="Mantenimiento"
+            className="w-full h-auto object-cover rounded-lg mx-auto md:w-[500px] md:h-[300px] lg:w-[600px] lg:h-[350px] xl:w-[700px] xl:h-[400px]"
+            loading="lazy"
+          />
+        </div>
+        <div className="lg:max-w-[600px] lg:mx-auto xl:max-w-[700px] flex flex-col justify-center">
           <h2 className="text-2xl md:text-3xl font-semibold mb-4">Mantenimiento</h2>
           <h3 className="text-xl font-bold mb-4 text-gray-600">Cambio de Aceite y Filtro</h3>
           <p className="text-lg leading-relaxed">
@@ -149,20 +152,9 @@ export default function Services() {
             para recordarte cuando estés en el tiempo de un nuevo cambio de aceite.
           </p>
         </div>
-        <div>
-          <Image
-            src="/servicios/oil.jpg"
-            width={700}
-            height={400}
-            alt="Mantenimiento"
-            className="w-full h-auto object-cover rounded-lg"
-            loading="lazy"
-          />
-        </div>
       </section>
-
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-16 mt-16">
-        <div>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-10 xl:px-16 mt-16">
+        <div className="order-last md:order-first flex flex-col justify-center">
           <h2 className="text-2xl md:text-3xl font-semibold mb-4">Seguimiento en Línea</h2>
           <h3 className="text-xl font-bold mb-4 text-gray-600">Bitácora de Mantenciones</h3>
           <p className="text-lg leading-relaxed">
@@ -170,107 +162,105 @@ export default function Services() {
             mantenciones en general.
           </p>
         </div>
-        <div>
+        <div className="text-center md:text-left">
           <Image
             src="/servicios/call.jpg"
             width={700}
             height={400}
             alt="Seguimiento en Línea"
-            className="w-full h-auto object-cover rounded-lg"
+            className="w-full h-auto object-cover rounded-lg mx-auto md:w-[500px] md:h-[300px] lg:w-[600px] lg:h-[350px] xl:w-[700px] xl:h-[400px]"
             loading="lazy"
           />
         </div>
       </section>
 
+      {/* Installation Section */}
       <section className="text-center mb-16 mt-16">
         <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-8">Instalaciones</h1>
       </section>
 
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-16 mt-16">
-  <div>
-    <h2 className="text-2xl md:text-3xl font-semibold mb-4 text-gray-800">Instalación de Inmovilizadores</h2>
-    <h3 className="text-xl font-bold mb-4 text-gray-600">Corta Corriente / Antiportazo</h3>
-    <p className="text-lg leading-relaxed text-gray-700">
-      En Rubinstein nos preocupa tu seguridad y la de tu vehículo. Por ello, ofrecemos la instalación de inmovilizadores
-      para proteger tu vehículo de robos o portonazos. Nuestro sistema es indetectable por delincuentes, inhackeable y
-      más seguro que los GPS.
-    </p>
-  </div>
-  <div>
-    <Image
-      src="/servicios/thief.jpg"
-      width={700}
-      height={400}
-      alt="Instalación de Inmovilizadores"
-      className="w-full h-auto object-cover rounded-lg shadow-lg"
-      loading="lazy"
-    />
-  </div>
-</section>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-10 xl:px-16 mt-16">
+        <div className="order-last md:order-first flex flex-col justify-center">
+          <h2 className="text-2xl md:text-3xl font-semibold mb-4 text-gray-800">Instalación de Inmovilizadores</h2>
+          <h3 className="text-xl font-bold mb-4 text-gray-600">Corta Corriente / Antiportazo</h3>
+          <p className="text-lg leading-relaxed text-gray-700">
+            En Rubinstein nos preocupa tu seguridad y la de tu vehículo. Por ello, ofrecemos la instalación de inmovilizadores
+            para proteger tu vehículo de robos o portonazos. Nuestro sistema es indetectable por delincuentes, inhackeable y
+            más seguro que los GPS.
+          </p>
+        </div>
+        <div className="text-center md:text-left">
+          <Image
+            src="/servicios/thief.jpg"
+            width={700}
+            height={400}
+            alt="Instalación de Inmovilizadores"
+            className="w-full h-auto object-cover rounded-lg shadow-lg mx-auto md:w-[500px] md:h-[300px] lg:w-[600px] lg:h-[350px] xl:w-[700px] xl:h-[400px]"
+            loading="lazy"
+          />
+        </div>
+      </section>
 
-<section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-16 mt-16">
-  <div>
-    <h2 className="text-2xl md:text-3xl font-semibold mb-4 text-gray-800">Alarma y Cierre</h2>
-    <h3 className="text-xl font-bold mb-4 text-gray-600">Alarma y Cierre Centralizado</h3>
-    <p className="text-lg leading-relaxed text-gray-700">
-      Si necesitas instalar una alarma de seguridad o renovar el cierre centralizado de tu vehículo, en Rubinstein te
-      ofrecemos la mejor opción para tu auto. Contáctanos para más detalles.
-    </p>
-  </div>
-  <div>
-    <Image
-      src="/servicios/alarm.jpg"
-      width={700}
-      height={400}
-      alt="Alarma y Cierre"
-      className="w-full h-auto object-cover rounded-lg shadow-lg"
-      loading="lazy"
-    />
-  </div>
-</section>
-
-<section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-16 mt-16">
-  <div>
-    <h2 className="text-2xl md:text-3xl font-semibold mb-4 text-gray-800">Radio y Cámara</h2>
-    <h3 className="text-xl font-bold mb-4 text-gray-600">Instalación de Radio Android y Cámara de Retroceso</h3>
-    <p className="text-lg leading-relaxed text-gray-700">
-      Moderniza tu radio y añade una cámara de retroceso para tu seguridad. En Rubinstein, tenemos una selección de
-      radios Android o te asesoramos en la compra de un modelo específico.
-    </p>
-  </div>
-  <div>
-    <Image
-      src="/servicios/radio.jpg"
-      width={700}
-      height={400}
-      alt="Radio y Cámara"
-      className="w-full h-auto object-cover rounded-lg shadow-lg"
-      loading="lazy"
-    />
-  </div>
-</section>
-
-<section className="grid grid-cols-1 mb-20 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-16 mt-16">
-  <div>
-    <h2 className="text-2xl md:text-3xl font-semibold mb-4 text-gray-800">Botones de Encendido</h2>
-    <h3 className="text-xl font-bold mb-4 text-gray-600">Botón Start/Stop Engine</h3>
-    <p className="text-lg leading-relaxed text-gray-700">
-      Moderniza tu vehículo con un botón de encendido. En Rubinstein realizamos la instalación del botón Start/Stop
-      Engine y ofrecemos garantía de satisfacción por nuestro trabajo.
-    </p>
-  </div>
-  <div>
-    <Image
-      src="/servicios/start.jpg"
-      width={700}
-      height={400}
-      alt="Botón Start/Stop Engine"
-      className="w-full h-auto object-cover rounded-lg shadow-lg"
-      loading="lazy"
-    />
-  </div>
-</section>
-
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-10 xl:px-16">
+        <div className="text-center md:text-left">
+          <Image
+            src="/servicios/alarm.jpg"
+            width={700}
+            height={400}
+            alt="Alarma y Cierre"
+            className="w-full h-auto object-cover rounded-lg mx-auto md:w-[500px] md:h-[300px] lg:w-[600px] lg:h-[350px] xl:w-[700px] xl:h-[400px]"
+            loading="lazy"
+          />
+        </div>
+        <div className="lg:max-w-[600px] lg:mx-auto xl:max-w-[700px] flex flex-col justify-center">
+          <h2 className="text-2xl md:text-3xl font-semibold mb-4">Alarma y Cierre</h2>
+          <h3 className="text-xl font-bold mb-4 text-gray-600">Alarma y Cierre Centralizado</h3>
+          <p className="text-lg leading-relaxed">
+             Si necesitas instalar una alarma de seguridad o renovar el cierre centralizado de tu vehículo, en Rubinstein te
+            ofrecemos la mejor opción para tu auto. Contáctanos para más detalles.
+          </p>
+        </div>
+      </section>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-10 xl:px-16 mt-16">
+        <div className="order-last md:order-first flex flex-col justify-center">
+          <h2 className="text-2xl md:text-3xl font-semibold mb-4">Radio y Cámara</h2>
+          <h3 className="text-xl font-bold mb-4 text-gray-600">Instalación de Radio Android y Cámara de Retroceso</h3>
+          <p className="text-lg leading-relaxed">
+             Moderniza tu radio y añade una cámara de retroceso para tu seguridad. En Rubinstein, tenemos una selección de
+            radios Android o te asesoramos en la compra de un modelo específico.
+          </p>
+        </div>
+        <div className="text-center md:text-left">
+          <Image
+            src="/servicios/radio.jpg"
+            width={700}
+            height={400}
+            alt="Radio y Cámara"
+            className="w-full h-auto object-cover rounded-lg mx-auto md:w-[500px] md:h-[300px] lg:w-[600px] lg:h-[350px] xl:w-[700px] xl:h-[400px]"
+            loading="lazy"
+          />
+        </div>
+      </section>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-10 xl:px-16">
+        <div className="text-center md:text-left">
+          <Image
+            src="/servicios/start.jpg"
+            width={700}
+            height={400}
+            alt="Botón Start/Stop Engine"
+            className="w-full h-auto object-cover rounded-lg mx-auto md:w-[500px] md:h-[300px] lg:w-[600px] lg:h-[350px] xl:w-[700px] xl:h-[400px]"
+            loading="lazy"
+          />
+        </div>
+        <div className="lg:max-w-[600px] lg:mx-auto xl:max-w-[700px] flex flex-col justify-center">
+          <h2 className="text-2xl md:text-3xl font-semibold mb-4">Botones de Encendido</h2>
+          <h3 className="text-xl font-bold mb-4 text-gray-600">Botón Start/Stop Engine</h3>
+          <p className="text-lg leading-relaxed">
+            Moderniza tu vehículo con un botón de encendido. En Rubinstein realizamos la instalación del botón Start/Stop
+            Engine y ofrecemos garantía de satisfacción por nuestro trabajo.
+          </p>
+        </div>
+      </section>
     </main>
   );
 }
