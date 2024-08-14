3593d3a687d2bd95d09e3fc2c96641bc0014ac10
Christopher Cortes <christopherctes@hotmail.com>
2024-08-14 19:56:01 -0400

    nav agenda added

--- app/ui/nav-links.tsx
+++ app/ui/nav-links.tsx
@@ -11,7 +11,7 @@ const links = [
   { name: 'Quienes Somos', href: '/quienes-somos' },
   { name: 'Nuestros Servicios', href: '/nuestros-servicios' },
   { name: 'Taller Multimarcas', href: '/taller-multimarcas' },
-  { name: 'Contacto', href: '/contacto' },
+  { name: 'Agenda', href: '/agenda' },
 ];
 
 export default function NavLinks() {
